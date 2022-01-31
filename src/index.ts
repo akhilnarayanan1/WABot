04429571be55f9f26b401afb645b759522713cb9
Akhil Narayanan <akhilnarayanan.work@gmail.com>
2022-01-31 21:56:16 +0530

    fixing bug

--- src/index.ts
+++ src/index.ts
@@ -11,12 +11,13 @@ socket.ev.on("messages.upsert", async (m: any) => {
     // Send Read Receipt
     await socket.sendReadReceipt(msg.key.remoteJid, msg.key.participant, [msg.key.id]);
   
-    // Only Read Messages with Prefix
-    const {command, args} = getUserCommandFromMessage(prefix, getUserMessage(msg));
-    const chartOrGroup = msg.key.remoteJid.endsWith("@g.us") ? "group" : "chat";
-  
     // Respond to messages which are NOT from Bot
     if (!(msg.key.fromMe)) {
+
+      // Only Read Messages with Prefix
+      const {command, args} = getUserCommandFromMessage(prefix, getUserMessage(msg));
+      const chartOrGroup = msg.key.remoteJid.endsWith("@g.us") ? "group" : "chat";
+
       switch (command) {
         case "help":
           await sendMessageWTyping({text: `yoooooooo this works ${chartOrGroup} ${args}`}, msg.key.remoteJid, socket);
